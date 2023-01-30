8a13b6485072cbab358e497ea152199aaa84527e
Erik Rose <erik@mozilla.com>
2023-01-29 20:27:45 -0500

    Indentation-based parser now works, passes tests, and is reasonably comprehensible!

--- dabble/indent_parser.py
+++ dabble/indent_parser.py
@@ -23,7 +23,7 @@ class TokenConst(object):
 
     def __init__(self, identity):
         self.identity = identity
-    
+
     def __repr__(self):
         return self.identity
 
@@ -38,8 +38,11 @@ def lex(text):
     """Break down a string into an iterable of tokens based on indentation in a
     scheme akin to the I-expressions presented in SRFI 49.
 
-    * Indenting starts a new list.
-    * The list continues until indentation goes back to the pre-indented level.
+    * Indenting starts a new list that functions as a block.
+    * The block list continues until indentation goes back to the pre-indented
+      level.
+    * Within a block, each line at the same level of indentation is its own
+      list.
     * One atom on a line is just an atom, not a 1-list. This will
       keep us from having to overnotate in the branches of ifs, which are
       common. And it will make our unary functions look like OCaml's--``foo
@@ -51,47 +54,76 @@ def lex(text):
     * Inconsistent indentation is an error. The whitespace characters that make
       up each indent must be either an addition to or a truncation of the ones
       in the indent above.
-    * Later: Indentation is ignored inside parens so you can use whitespace for
-      human comprehension in non-machine-readable ways.
-
-    The caller should create the top-level list all the yielded tokens go into.
+    * Indentation is ignored inside parens so you can use whitespace for human
+      comprehension in non-machine-readable ways.
 
     """
-    old_indent = ''
+    old_indent = None
     # A stack of lengths of indents of indentation-based lists that enclose this
-    # one:
-    encloser_starts = []
-    ever_opened_anything = False
+    # one. There is an implicit top-level list enclosing the whole program. This
+    # is at indent level 0.
+    at = []
     # How many paren pairs we're inside:
     enclosing_parens = 0
 
     for match in token_pattern.finditer(text):
         type = match.lastgroup
+        print(type, match.group().strip(), ':')
         if type == 'dent':
             if enclosing_parens <= 0:  # Ignore indentation inside parens.
                 new_indent = match.group('dent')
-                if new_indent == old_indent:
-                    if ever_opened_anything:
-                        yield CLOSER
+                if not at:  # BOF
+                    print('BOF: [[')
+                    at.append(0)
+                    yield OPENER
+                    yield OPENER
+                elif new_indent == old_indent:  # same dent
+                    print('samedent: ][')
+                    yield CLOSER
+                    # Open the new (sibling) list we're about
+                    # to start making:
+                    yield OPENER
                 elif new_indent.startswith(old_indent):  # indent
-                    encloser_starts.append(len(old_indent))
+                    print('indent and open the line: OPENER, OPENER')
+                    at.append(len(new_indent))
+                    # Open the new (child) block we're about
+                    # to start making:
+                    yield OPENER
+                    # And open the first line of the block:
+                    yield OPENER
                 elif old_indent.startswith(new_indent):  # outdent
+                    assert len(at) >= 2, "How can there be no encloser if we're outdenting?"
                     # You get one just for ending the line: this ends the
                     # current line's list.
+                    print('    outdent or partial ]')
                     yield CLOSER
-                    # Then you get another for each level you outdent:
-                    while (encloser_starts and
-                           encloser_starts[-1] >= len(new_indent)):
-                        encloser_starts.pop()
+                    if len(new_indent) <= at[-2]:  # full outdent  # TODO: Don't just measure and keep track of lengths of indents, or we won't be able to support mixed tabs and spaces. Or will we? Maybe it just works, since indenting always adds length and dedenting always subtracts it.
+                        print('full outdent')
+                        # Then you get another for each level you outdent:
+                        while len(at) >= 2 and len(new_indent) <= at[-2]:
+                            print(f'    pop level {at[-1]} ]]')
+                            yield CLOSER
+                            yield CLOSER
+                            at.pop()
+                        # Now we're going to start a new line, because otherwise
+                        # this would be EOF and be taken care of down at the
+                        # bottom of this proc.
+                        print('    and open: [')
+                        yield OPENER
+                    else:  # partial outdent
+                        # We outdented from the previous line but not out to the
+                        # level of the previous indent.
+
+                        print('partial outdent, so close block: ]')
+                        # End the block:
                         yield CLOSER
+                        # Pop the indentation into the block we just closed:
+                        at.pop()
                 else:
                     raise LexError("Indentation was not consistent. The whitespace characters that make up each indent must be either an addition to or a truncation of the ones in the indent above. You can't just swap out tabs for spaces suddenly.")
 
-                # Open the new (child, sibling, or outdented) list we're about
-                # to start making:
-                yield OPENER
-                ever_opened_anything = True
                 old_indent = new_indent
+
         elif type == 'paren':
             enclosing_parens += 1
             yield '('
@@ -109,38 +141,59 @@ def lex(text):
         elif type == 'unmatched':
             raise LexError('Unrecognized token: "%s".' % match.group())
 
-    for _ in encloser_starts:
+    for indent in at:
+        print(f'EOF: ] to end line and block {indent}: ', end='')
         yield CLOSER
-    if ever_opened_anything:
         yield CLOSER
 
 
-def _parse_core(token_iter, closer_we_are_waiting_for):
+def _parse_list(token_iter, return_at):
+    """Start parsing the token stream at a list opener, either OPENER or '('.
+    Parse until we reach the matching closer, then return the parsed list plus a
+    bool representing whether we collapsed the list from a 1-list to an atom
+    (and thus it shouldn't be collapsed further)."""
     ret = []
+    wrong_closer = ')' if return_at is OPENER else CLOSER
+    # Whether we already collapsed the single list inside ``ret`` to an atom:
+    collapsed = False
     for token in token_iter:
         if token in (OPENER, '('):
-            ret.append(_parse_core(token_iter, CLOSER if token is OPENER else ')'))
-        elif token is closer_we_are_waiting_for:
+            awaited_closer = CLOSER if token is OPENER else ')'
+            l, collapsed = _parse_list(token_iter, awaited_closer)
+            ret.append(l)
+        elif token == return_at:
             # A single atom on a line is just the atom, not a 1-list:
-            if len(ret) == 1 and closer_we_are_waiting_for is CLOSER:
+            if len(ret) == 1 and not isinstance(ret[0], list) and return_at is CLOSER and not collapsed:
                 # This is another way of saying we saw the token sequence
                 # OPENER, atom, CLOSER.
-                return ret[0]
-            return ret
-        elif token is CLOSER:
-            # Superfluous end parentheses are caught earlier, in the lexer.
+                return ret[0], True
+            return ret, False
+        elif token == wrong_closer:
+            # Superfluous end parentheses (that is, missing dedents) are caught
+            # earlier, in the lexer.
             raise LexError("You're missing an end parenthesis.")
         else:
             ret.append(token)
-    return ret
+    return ret, False
 
 
 def parse(tokens):
     """Turn the token stream from the lexer into a parse tree.
 
+    parse() won't work unless the stream starts with an OPENER and ends with a
+    CLOSER.
+
     expr = atom | list
     atom = int | word
-    list = OPENER expr* CLOSER
+    list = (OPENER expr* CLOSER) | ('(' expr* ')')
 
     """
-    return _parse_core(iter(tokens), CLOSER)
+    token_iter = iter(tokens)
+
+    # Keep _parse_list() from adding an additional nested list inside the one it
+    # implicitly makes. It'll still happily match and consume the ending CLOSER.
+    assert next(token_iter) is OPENER
+
+    l, collapsed = _parse_list(token_iter, CLOSER)
+    return l
+    # TODO: Throw a fit if there are leftover tokens, meaning we prematurely closed all enclosers.
--- dabble/tests/test_indent_parser.py
+++ dabble/tests/test_indent_parser.py
@@ -17,8 +17,10 @@ same dent
 # Comment
 """
     assert lexed(text) == [
-        OPENER, 'some', 'dent', CLOSER,
-        OPENER, 'same', 'dent', CLOSER]
+        OPENER,
+            OPENER, 'some', 'dent', CLOSER,
+            OPENER, 'same', 'dent', CLOSER,
+        CLOSER]
 
 
 def test_outdent_closer_count():
@@ -30,12 +32,14 @@ def test_outdent_closer_count():
   c
 d"""
     assert lexed(text) == [
-        OPENER, 'a',
-            OPENER, 'b',
-                OPENER, 'c', CLOSER,
+        OPENER,
+            OPENER, 'a',
+                OPENER, OPENER, 'b',
+                    OPENER, OPENER, 'c', CLOSER, CLOSER,
+                CLOSER, CLOSER,
             CLOSER,
-        CLOSER,
-        OPENER, 'd', CLOSER]
+            OPENER, 'd', CLOSER,
+        CLOSER]
 
 
 def test_close_indents_at_eof():
@@ -45,12 +49,14 @@ def test_close_indents_at_eof():
   c
    d"""
     assert lexed(text) == [
+        OPENER,
         OPENER, 'a',
-            OPENER, 'b',
-                OPENER, 'c',
-                    OPENER, 'd', CLOSER,
-                CLOSER,
-            CLOSER,
+            OPENER, OPENER, 'b',
+                OPENER, OPENER, 'c',
+                    OPENER, OPENER, 'd', CLOSER, CLOSER,
+                CLOSER, CLOSER,
+            CLOSER, CLOSER,
+        CLOSER,
         CLOSER]
 
 
@@ -69,11 +75,20 @@ def test_everything_indented():
                            OPENER, 'b', CLOSER]
 
 
-def test_basics():
-    #assert parsed('"hi"') == '"hi"'
+def test_lex_basics():
+    """Make sure some basic contruct lex (or properly fail to)."""
+    assert lexed('word***') == [OPENER, OPENER, 'word***', CLOSER, CLOSER]
+    assert lexed('(8 9 abc+)') == [OPENER, OPENER, '(', 8, 9, 'abc+', ')', CLOSER, CLOSER]
+    assert lexed('(1 2 (3 4))') == [OPENER, OPENER, '(', 1, 2, '(', 3, 4, ')', ')', CLOSER, CLOSER]
+    with raises(LexError):
+        lexed(')')
+
+
+def test_parse_basics():
+    #assert parsed('"hi"') == '"hi"'  # strings not supported yet
     assert parsed('word***') == ['word***']
-    assert parsed('(8 9 abc+)') == [[8, 9, 'abc+']]
-    assert parsed('(1 2 (3 4))') == [[1, 2, [3, 4]]]
+    assert parsed('(8 9 abc+)') == [[[8, 9, 'abc+']]]
+    assert parsed('(1 2 (3 4))') == [[[1, 2, [3, 4]]]]
     with raises(LexError):
         parsed(')')
 
@@ -89,15 +104,16 @@ def test_whitespace_only_lines_are_skipped():
     text = """a
 
      b 0"""
-    assert parsed(text) == [['a', ['b', 0]]]
+    assert parsed(text) == [['a', [['b', 0]]]]
 
 
 def parsed(text):
+    """Lex and parse one or more well-formed lines of code."""
     return parse(lex(text))
 
 
 def test_parse_single_line():
-    assert parse([OPENER, 4, 5, CLOSER]) == [[4, 5]]
+    assert parse([OPENER, OPENER, 4, 5, CLOSER, CLOSER]) == [[4, 5]]
 
 
 def test_parse_indented():
@@ -105,23 +121,50 @@ def test_parse_indented():
  b
   c 0
 d"""
-    assert parsed(text) == [['a', ['b', ['c', 0]]], 'd']
+    assert lexed(text) == [
+        OPENER,
+            OPENER,
+                'a',
+                OPENER,
+                    OPENER, 'b',
+                        OPENER,
+                            OPENER, 'c', 0, CLOSER,
+                        CLOSER,
+                    CLOSER,
+                CLOSER,
+            CLOSER,
+            OPENER, 'd', CLOSER,  # This one will be collapsed from a 1-list to an atom.
+        CLOSER
+    ]
+    assert parsed(text) == [['a', [['b', [['c', 0]]]]], 'd']
 
 
 def test_single_atom_is_not_list():
-    """A single atom on a list should be just the atom, not a 1-list."""
+    """A single atom on a line should be just the atom, not a 1-list."""
+    # The list here is the all-containing list for the whole program:
     assert parsed('2') == [2]
-    assert parsed("""
+    text = """
 if smoo
     0
-    1""") == [['if', 'smoo', 0, 1]]
+    1"""
+    assert lexed(text) == [
+        OPENER,
+            OPENER, 'if', 'smoo',
+                OPENER,
+                    OPENER, 0, CLOSER,
+                    OPENER, 1, CLOSER,
+                CLOSER,
+            CLOSER,
+        CLOSER,
+    ]
+    assert parsed(text) == [['if', 'smoo', [0, 1]]]
 
 
 def test_parens():
     assert parsed('1 (2 3 4) 5') == [[1, [2, 3, 4], 5]]
     assert parsed("""
 1
-    2 (3 4 5)""") == [[1, [2, [3, 4, 5]]]]
+    2 (3 4 5)""") == [[1, [[2, [3, 4, 5]]]]]
 
 
 def test_single_atom_in_parens_is_a_list():
@@ -130,4 +173,132 @@ def test_single_atom_in_parens_is_a_list():
     text = """
 fun (x)
     2"""
-    assert parsed(text) == [['fun', ['x'], 2]]
+    # The brackets around the 2 are for the block. The ones for the line that
+    # would make it a 1-list are gone.
+    assert parsed(text) == [['fun', ['x'], [2]]]
+
+
+def test_indentation_ignored_inside_parens():
+    """Indentation inside parentheses shouldn't count for anything."""
+    assert parsed("""
+1
+  2 (3
+4
+  5 6
+7) 8""") == [[1, [[2, [3, 4, 5, 6, 7], 8]]]]
+
+
+def test_partial_outdent():
+    """Make sure we don't consider it an outdent until we outdent all the way to
+    the enclosing indent level."""
+    assert lexed("""
+if foo
+    1
+  else
+    0""") == [
+    OPENER,
+        OPENER,
+            'if', 'foo',
+                OPENER, OPENER, 1, CLOSER, CLOSER,
+            'else',
+                OPENER, OPENER, 0, CLOSER, CLOSER,
+        CLOSER,
+    CLOSER]
+# I don't think an indent after a partial outdent is worth an indent.
+
+
+def test_impartial_outdent():
+    """Contrast with test_partial_outdent to learn in what situations closer behavior should be different."""
+    assert lexed("""
+if foo
+    1
+    0""") == [OPENER,
+              OPENER, 'if', 'foo', OPENER,
+              OPENER, 1, CLOSER,
+              OPENER, 0, CLOSER,
+              CLOSER, CLOSER,
+              CLOSER]
+
+
+def test_ending_at_partial_outdent():
+    assert lexed("""
+if foo
+    1
+  else""") == [
+    OPENER,
+        OPENER,
+            'if', 'foo',
+                OPENER, OPENER, 1, CLOSER, CLOSER,
+            'else',
+        CLOSER,
+    CLOSER]
+
+
+def test_one_liner():
+    """Make sure we emit a trailing CLOSER for one-liners."""
+    assert lexed("""foo""") == [OPENER, OPENER, 'foo', CLOSER, CLOSER]
+
+
+# if foo
+#     1
+#   else  # Is this an encloser_start we need to close eventually? Why or why not?
+#     0
+# 
+# vs.
+# 
+# if foo
+#     1
+#   if bar  # partial outdent means a continuation of enclosing list. partial means not all the way to enclosing level.
+#     2
+# 
+# if foo
+#     1 2
+#   if bar
+#  if baz
+#     2 3
+# should mean [if foo [1 2] if bar if baz [2 3]]
+# 
+# if foo
+#     1
+#   else  # Is this an encloser_start we need to close eventually? Why or why not? Never. You need to close due to a thing being INDENTED, like the "if" that immediately follows. There is no indent here; it is solely and unambiguously a partial outdent.
+#     if bar
+#         3
+#     0
+
+
+# if foo
+#     0
+#   else
+#     1
+#   otherwise
+#     2
+# # means [if foo [0] else [1] otherwise [2]]
+# 
+# if foo
+#     0
+#   else
+#     1
+#  otherwise
+#     2
+# # means the same
+# 
+# if foo
+#     0
+#   else
+#     1
+#    otherwise
+#     2
+# # means the same
+
+
+# Does…
+#     if foo
+#         0
+#     [[if foo [[0]]]]  # with atoms as lists (as the lexer should render them), not mere atoms
+# …need fewer closers than…
+#     if foo
+#         0
+#       else
+#         1
+#     [[if foo [[0]] else [[1]]]]
+# …? [No.] If so, why? Does it *get* fewer closers? [No.] That could crack this problem wide open.
